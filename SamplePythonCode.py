89eecbe6874611acfebfab6b4cd1e86ab8eef632
Tanvi Chettiyar <tanvi.chettiyar@gmail.com>
2024-11-20 21:49:04 -0500

    Imported Pandas

--- SamplePythonCode.py
+++ SamplePythonCode.py
@@ -1,5 +1,14 @@
 import datetime
 print(datetime.datetime.now())
-import sys
-import pathlib
-print(pathlib.Path.absolute)
+
+import pandas
+df = pandas.read_csv("/Users/tanvi_rajkumar/Documents/GitRepo/automatic-system/student.txt")
+print(df)
+## print(df.filter(df["grade"]<5))
+faildf = df.query("grade<5")
+faildf.to_csv("/Users/tanvi_rajkumar/Documents/GitRepo/automatic-system/out.csv",
+              sep="|",
+              index=False,
+              header=True, 
+              columns=["name","subject","grade"],
+              mode="w")
