c43be4fecbfb3f5b5d95ae8dc0941a3369ed14c9
Tanvi Chettiyar <tanvi.chettiyar@gmail.com>
2024-12-09 16:52:16 -0500

    Added more filter functions

--- SamplePythonCode.py
+++ SamplePythonCode.py
@@ -6,6 +6,8 @@ df = pandas.read_csv("/Users/tanvi_rajkumar/Documents/GitRepo/automatic-system/s
 print(df)
 ## print(df.filter(df["grade"]<5))
 faildf = df.query("grade<5")
+faildf2 = df.loc(df['grade'] < 5, ['id','name'])
+faildf3 = df[df['grade'].isin([0,1,2,3,4])]
 faildf.to_csv("/Users/tanvi_rajkumar/Documents/GitRepo/automatic-system/out.csv",
               sep="|",
               index=False,
