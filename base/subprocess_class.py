d27695e73bbd02185a45aa5664336d1cf5ed71ff
Tanvi Chettiyar <tanvi.chettiyar@gmail.com>
2025-01-17 17:42:52 -0500

    edited subprocess class

--- base/subprocess_class.py
+++ base/subprocess_class.py
@@ -1,4 +1,4 @@
-import subprocess
+from subprocess import run
 from typing import List
 
 class UsingSubprocess:
@@ -9,14 +9,14 @@ class UsingSubprocess:
         self.tablenames = tablenames
 
     def execute(self):
-        subprocess.run(f'psql -d {self.database} -U {self.user}')
-        subprocess.run(f"""create foreign table if not exists {self.tablenames[0]} (id INTEGER, name TEXT, subject TEXT, grade INTEGER)
+        run(f'psql -d {self.database} -U {self.user}')
+        run(f"""create foreign table if not exists {self.tablenames[0]} (id INTEGER, name TEXT, subject TEXT, grade INTEGER)
                        server file_server 
                        options(filename '{self.filepath}', format 'csv', delimiter ',', header 'true');""")
-        subprocess.run('commit')
-        subprocess.run('echo $?')
-        subprocess.run(f"insert into {self.tablenames[1]} select * from {self.tablenames[0]}")
-        subprocess.run('commit')
-        subprocess.run('echo $?')
-        subprocess.run(f"drop foreign table if exists {self.tablenames[0]}")
-        subprocess.run("\q")
+        run('commit')
+        run('echo $?')
+        run(f"insert into {self.tablenames[1]} select * from {self.tablenames[0]}")
+        run('commit')
+        run('echo $?')
+        run(f"drop foreign table if exists {self.tablenames[0]}")
+        run("\q")
